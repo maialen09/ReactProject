8ed8b5d84cf197f34a9962eb161836a83adcf900
Maialen Torre <maialentorre09@gmail.com>
2025-09-23 05:59:03 -0400

    Github action corrections of building

--- react-phase-1/vite.config.ts
+++ react-phase-1/vite.config.ts
@@ -1,11 +1,18 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-  }
+  },
+  
 })
